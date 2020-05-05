f55a3fb2a30bda75d1c0c5b89123ff4ebdd9ecaf
karoliina.ruutsofia@gmail.com <43740088+karoliah@users.noreply.github.com>
2020-05-05 15:26:38 +0300

    kokeilu

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ const userRoute = require('./routes/userRoute');
 app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-/*if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     require('./remote')(app, port);
 } else {
     require('./localhost')(app, port);
-}*/
+}
 app.use(express.static('public'));
 app.use(express.static('uploads'));
 app.use('/thumbnails', express.static('thumbnails'));
 
-app.use('/auth', authRoute);
-app.use('/photo', passport.authenticate('jwt', {session: false}), photoRoute); //passport.authenticate('jwt', {session: false}),
-app.use('/user',  passport.authenticate('jwt', {session: false}), userRoute); //passport.authenticate('jwt', {session: false}),
+app.use('/app', authRoute);
+app.use('/app', passport.authenticate('jwt', {session: false}), photoRoute); //passport.authenticate('jwt', {session: false}),
+app.use('/app',  passport.authenticate('jwt', {session: false}), userRoute); //passport.authenticate('jwt', {session: false}),
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
